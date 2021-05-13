4fc96ff7da9c81953d78be8da04a937b89ced05f
Benilton <bsa.ele@uea.edu.br>
2021-05-13 11:23:27 -0500

    refact: rename functions and variables

--- app.ino
+++ app.ino
@@ -1,33 +1,34 @@
-#include <Thread.h>
 #include <ThreadController.h>
 #include <idDHT11.h>
+#include <Thread.h>
 
 
-#define buzzer_pin 3       
-#define idDHT11pin 2       
 #define idDHT11intNumber 0 
+#define readingTime 1000
 #define threadSleep 700
+#define idDHT11pin 2       
+#define buzzer_pin 3       
 
 
-void minTemperature();
-void maxTemperature();
-void dht11_wrapper();
+void dht11Wrapper();
+void minCelsius();
+void maxCelsius();
 void outOfRange();
 void loopDHT();
 
 
-idDHT11 DHT11(idDHT11pin, idDHT11intNumber, dht11_wrapper);   
+idDHT11 DHT11(idDHT11pin, idDHT11intNumber, dht11Wrapper);   
 ThreadController threadController = ThreadController();
 ThreadController groupOfThreads = ThreadController();
-Thread minTemperatureThread = Thread();
-Thread maxTemperatureThread = Thread();
+Thread minCelsiusThread = Thread();
+Thread maxCelsiusThread = Thread();
 Thread outOfRangeThread = Thread();
 
 
-float temperaturaC;
-float max_temp;
-float min_temp;
 static bool controller = false;
+float maxValueCelsius;
+float minValueCelsius;
+float celsius;
 
 
 void setup(){
@@ -47,8 +48,8 @@ void loop(){
 
 
 void outOfRange(){
-  if (temperaturaC < 25.2 || temperaturaC > 32.6){
-    Serial.print(temperaturaC, 2); // debug
+  if (celsius < 25.2 || celsius > 32.6){
+    Serial.print(celsius, 2); // debug
     Serial.println(" °C"); // debug
     delay(200);
     digitalWrite(buzzer_pin, HIGH); 
@@ -56,61 +57,60 @@ void outOfRange(){
     digitalWrite(buzzer_pin, LOW);   
   }
   else{
-    Serial.print(temperaturaC, 2); // debug
+    Serial.print(celsius, 2); // debug
     Serial.println(" °C"); // debug
   }
 }
 
 
-void minTemperature(){
-  if (temperaturaC < min_temp){
-    min_temp = temperaturaC;
-    Serial.print(min_temp, 2); // debug
-    Serial.println(" °C MIN_TEMP"); // debug
+void minCelsius(){
+  if (celsius < minValueCelsius){
+    minValueCelsius = celsius;
+    Serial.print(minValueCelsius, 2); // debug
+    Serial.println(" °C minValueCelsius"); // debug
   }  
 }
 
 
-void maxTemperature(){
-  if (temperaturaC > max_temp){
-    max_temp = temperaturaC;
-    Serial.print(max_temp, 2); // debug
-    Serial.println(" °C MAX_TEMP"); // debug
+void maxCelsius(){
+  if (celsius > maxValueCelsius){
+    maxValueCelsius = celsius;
+    Serial.print(maxValueCelsius, 2); // debug
+    Serial.println(" °C maxValueCelsius"); // debug
   } 
 }
 
 
 void createThread(){
-  minTemperatureThread.setInterval(threadSleep);
-  maxTemperatureThread.setInterval(threadSleep);
+  minCelsiusThread.setInterval(threadSleep);
+  maxCelsiusThread.setInterval(threadSleep);
   outOfRangeThread.setInterval(threadSleep);
   
-  minTemperatureThread.onRun(minTemperature);
-  maxTemperatureThread.onRun(maxTemperature);
+  minCelsiusThread.onRun(minCelsius);
+  maxCelsiusThread.onRun(maxCelsius);
   outOfRangeThread.onRun(outOfRange);
 }
 
 
 void createGroup(){
-  groupOfThreads.add(&minTemperatureThread);
-  groupOfThreads.add(&maxTemperatureThread);
+  groupOfThreads.add(&minCelsiusThread);
+  groupOfThreads.add(&maxCelsiusThread);
   groupOfThreads.add(&outOfRangeThread);
   
   threadController.add(&groupOfThreads);
 }
 
 
-void dht11_wrapper() {
+void dht11Wrapper() {
   DHT11.isrCallback();
 }
 
 
 void loopDHT() {
-  #define tempoLeitura 1000
-  static unsigned long delayLeitura = millis() + tempoLeitura + 1;
+  static unsigned long readingDelay = millis() + readingTime + 1;
   static bool request = false;
 
-  if ((millis() - delayLeitura) > tempoLeitura) { 
+  if ((millis() - readingDelay) > readingTime) { 
       if (!request) {
         DHT11.acquire(); 
         request = true;
@@ -123,23 +123,23 @@ void loopDHT() {
     float valor = DHT11.getCelsius();
      
     if (!isnan(valor)) {             
-      temperaturaC = valor;
+      celsius = valor;
 
       if (!controller){   
-        min_temp = temperaturaC;
-        max_temp = temperaturaC;
+        minValueCelsius = celsius;
+        maxValueCelsius = celsius;
     
-        Serial.print(min_temp, 2); // debug
+        Serial.print(minValueCelsius, 2); // debug
         Serial.println(" °C MIN init"); // debug
     
-        Serial.print(max_temp, 2); // debug
+        Serial.print(maxValueCelsius, 2); // debug
         Serial.println(" °C MAX init"); // debug
         
         controller = true;
       }
     }
 
-    delayLeitura = millis();     
+    readingDelay = millis();     
 
   }
 }
